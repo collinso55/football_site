4cbbd553b9fd59652556d2b13a98db534940dec6
tamanjong collins <collinstamanjong21@gmail.com>
2025-11-12 03:15:53 +0100

    top leagues

---
+++ data/leagueFlags.js
@@ -0,0 +1,28 @@
+export const leagueFlags = {
+  // Domestic leagues
+  "Premier League": "gb",
+  "La Liga": "es",
+  "Bundesliga": "de",
+  "Serie A": "it",
+  "Ligue 1": "fr",
+  "Eredivisie": "nl",
+  "Primeira Liga": "pt",
+  "Süper Lig": "tr",
+  "Jupiler Pro League": "be",
+  "Ekstraklasa": "pl",
+  "1. Liga": "cz",
+  "Scottish Premiership": "gb",
+  "Liga I": "ro",
+  "NB I": "hu",
+  "Premier Liga": "ru",
+  "Allsvenskan": "se",
+  "Eliteserien": "no",
+  "Super League": "ch",
+
+  // International competitions → use logo URLs
+  "Champions League": "https://upload.wikimedia.org/wikipedia/en/8/87/UEFA_Champions_League_logo_2.svg",
+  "Europa League": "https://upload.wikimedia.org/wikipedia/en/9/9e/UEFA_Europa_League_logo.svg",
+  "Conference League": "https://upload.wikimedia.org/wikipedia/en/7/7e/UEFA_Conference_League_logo.svg",
+  "UEFA Nations League": "https://upload.wikimedia.org/wikipedia/en/f/f6/UEFA_Nations_League_logo.svg",
+  "South America": "https://upload.wikimedia.org/wikipedia/commons/9/99/CONMEBOL_logo.svg",
+};
